f3a6636e5500dfc76b0d06a256b3dfca1dd949ad
karim darakji <karim@jaypayagency.com>
2021-09-02 13:34:20 +0300

    t

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,10 @@ export default function Header() {
 
     const [open, setOpen] = useState(false)
 
+    const langD = ['English','Português','Español','简体中文']
+
+    const langM = ['EN','PT','ES','CN']
+
     const ref = useRef()
 
     useClickAway(ref, () => setOpen(false));
@@ -25,13 +29,13 @@ export default function Header() {
                             <div  className="w-50 language">
                                 <div ref={ref} className="dropdown">
                                 <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" onClick={() => setOpen(!open)}>
-                                    <img src={en} alt="language" /> EN
+                                    <img src={en} alt="language" /> {window.innerWidth > 800 ? langD[0] : langM[0]}
                                 </button>
                                 {open &&
                                 <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a className="dropdown-item pt" href="/pt/social">PT</a></li>
-                                <li><a className="dropdown-item es" href="/es/social">ES</a></li>
-                                <li><a className="dropdown-item zh" href="/zh/social">CN</a></li>
+                                <li><a className="dropdown-item pt" href="/pt/social">{window.innerWidth > 800 ? langD[1] : langM[1]}</a></li>
+                                <li><a className="dropdown-item es" href="/es/social">{window.innerWidth > 800 ? langD[2] : langM[2]}</a></li>
+                                <li><a className="dropdown-item zh" href="/zh/social">{window.innerWidth > 800 ? langD[3] : langM[3]}</a></li>
                                 </ul>
                                 }
                                 </div>
